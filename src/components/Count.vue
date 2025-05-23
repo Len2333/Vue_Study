338c099a3e86f3ea3148bc1c7e8b65adc47c7bbe
Len_legion <1059171203@qq.com>
2025-05-23 19:54:26 +0800

    5.23

---
+++ src/components/Count.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="count">
+        
+    </div>
+
+</template>
+
+
+<script setup lang="ts" name="Count">
+
+</script>
+
+<style scoped>
+.count {
+    background-color: aqua;
+    padding: 10px;
+    border-radius: 10px;
+    box-shadow: 0 0 1;
+}
+</style>
